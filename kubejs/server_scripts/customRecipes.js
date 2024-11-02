0554a037f29abe6b70ba0ddb4dcfb3ea4148c7a9
virtualonno <virtualonno@gmail.com>
2024-11-02 15:59:24 +0100

    revert, remove waterframes for testing

--- kubejs/server_scripts/customRecipes.js
+++ kubejs/server_scripts/customRecipes.js
@@ -391,7 +391,12 @@ ServerEvents.recipes( event => {
 	trappedIronChest('ironchest:gold_chest', 'ironchest:trapped_gold_chest')
 	trappedIronChest('ironchest:diamond_chest', 'ironchest:trapped_diamond_chest')
 
-	
+	event.shapeless(
+		Item.of('minecraft:bamboo', 9),
+		[
+			'quark:bamboo_block'
+		]
+	)
 	
 	
 	
